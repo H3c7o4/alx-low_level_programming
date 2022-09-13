c8f2164d81873969dc7f4115cf7279b9764b99d1
H3c7o4 <hectorvladitok@gmail.com>
2022-09-13 04:08:30 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * The program prints _putchar
+ *
+ */
+
+int main(void)
+{
+	int i=0;
+	char strng_ptchr[8]="_putchar";
+
+	while (i<8)
+	{
+		_putchar(strng_ptchr[i]);
+		i++
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Functions, nested loops !
+
+This directory is about my learning program in ALX.
+It is related with the C programming language and develop the usage of functions and nested loops.
