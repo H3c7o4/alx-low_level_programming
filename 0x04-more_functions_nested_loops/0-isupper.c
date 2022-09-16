e2b7803570772f3df21dd4762eb2ead33adce184
H3c7o4 <hectorvladitok@gmail.com>
2022-09-16 02:40:11 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper(int c) - Returns 1 if c is uppercase and Returns 0 otherwise
+ * _isupper - Returns 1 if c is uppercase and Returns 0 otherwise
  * @c: input integer
  * Return: int.
  */
