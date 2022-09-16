46e06c2737c9281be344cd4ee42bbd63d62ef191
H3c7o4 <hectorvladitok@gmail.com>
2022-09-16 08:44:02 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,6 @@ void print_number(int n)
 	int i;
 	int aux = 1;
 
-
 	if (n < 0)
 	{
 		n = n * -1;
@@ -29,7 +28,6 @@ void print_number(int n)
 		}
 		_putchar((num2 / aux) + '0');
 		aux = aux / 10;
-
 
 		for (i = aux; i >= 1; i = i / 10)
 			_putchar((num2 / i) % 10 + '0');
