01c8f726a65c0928c53f745ca754d19eb06642c0
H3c7o4 <hectorvladitok@gmail.com>
2022-09-16 02:19:41 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _isupper(int c) - Returns 1 if c is uppercase and Returns 0 otherwise
+ * @c: input integer
+ * Return: int.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		Return (1);
+	else
+		Return (0);
+}
