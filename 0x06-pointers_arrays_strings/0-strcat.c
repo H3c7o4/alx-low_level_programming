85a2d6081db8f1153aa5ab959e086a61403804d1
H3c7o4 <hectorvladitok@gmail.com>
2022-09-22 05:52:30 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	while (*(dest + 1) != '\0')
+	while (*(dest + i) != '\0')
 		i++;
 	while (*(src + j) != '\0')
 	{
