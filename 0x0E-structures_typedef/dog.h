f4db690c194ed3aaf0492d032eb08aa3659b6640
H3c7o4 <hectorvladitok@gmail.com>
2022-10-10 06:06:14 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog -This is a structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description:  This structure is created to collect datas from dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
