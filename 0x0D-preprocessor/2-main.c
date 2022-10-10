90d8ae6d8bb270313c65bc8eb7d017b656dc4625
H3c7o4 <hectorvladitok@gmail.com>
2022-10-10 04:08:18 -0700

    Task 2 complete

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Function that prints the name of the file
- * it was compiled from, followed by a new line.
+ * it was compiled from, followed by a new line
  * Return: 0 or 1
  */
 
