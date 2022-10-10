1910276cfb838e978488f760643b834f9ba57d6c
H3c7o4 <hectorvladitok@gmail.com>
2022-10-10 03:38:43 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints the name of the file
+ *  it was compiled from, followed by a new line.
+ *  Returns: 0 or 1
+ */
+
+int main(void)
+{
+	printf("%s", _FILE_);
+}
