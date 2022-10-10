80f64e20e4d7225eb24e7974d59b243add3cb55f
H3c7o4 <hectorvladitok@gmail.com>
2022-10-10 03:57:29 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Function that prints the name of the file
- *  it was compiled from, followed by a new line.
- *  Returns: 0 or 1
+ * it was compiled from, followed by a new line.
+ * Return: 0 or 1
  */
 
 int main(void)
