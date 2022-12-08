5b8c0c9e25d5f0e857eb88cf53f5a3cd658b872d
H3c7o4 <hectorvladitok@gmail.com>
2022-12-08 12:58:47 +0100

    Adding a new file

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the end of a dlistint_t list.
+ * @head: head of the list
+ * @n: number to add in the new node
+ * Return: The address of the new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *cur = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+
+		if (!(*head))
+			*head = new_node;
+		else
+		{
+			while (cur->next)
+			       cur = cur->next;
+			cur->next = new_node;
+			new_node->prev = cur;
+		}
+	}
+	return (new_node);
+}
