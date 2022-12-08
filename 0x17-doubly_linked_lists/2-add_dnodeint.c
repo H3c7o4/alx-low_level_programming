6e4424a2fc233bcd402332a56a4788787d09312b
H3c7o4 <hectorvladitok@gmail.com>
2022-12-08 12:06:35 +0100

    Adding a new file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_t *add_dnodeint(dlistint_t **head, const int n) - function that
+ * add_dnodeint - function that
  * adds a new node at the beginning of a dlistint_t list.
  * @head: head of the list
  * @n: Value of the new node
