66e1c2ec6efd24855a0611777d278d733cb95ba4
H3c7o4 <hectorvladitok@gmail.com>
2023-04-12 11:44:56 +0200

    Updating the 1-binary.c file

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,29 @@
 #include"search_algos.h"
 
 /**
+ * print_array - prints the array of ints, seperated with commas
+ * @array: a pointer to the array to print
+ * @first: the first index to print
+ * @last: the last index to print
+ * Return: the values of the list
+ */
+
+void print_array(int *array, size_t first, size_t last)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = first; i <= last; i++)
+	{
+		if (i != first)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
+
+/**
  * binary_search - function that searches for a value in a sorted
  * array of integers using the Binary search algorithm
  * @array: input sorted array
@@ -12,20 +35,23 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int L = 0;
-	unsigned int m;
+	size_t i = 0, first = 0, last = size - 1;
 
-	if (array == NULL)
-		return (-1);
-	while (size >= L)
+	if (array)
 	{
-		m = (L + size) / 2;
-		if (value > array[m])
-			L = m + 1;
-		else if (value < array[m])
-			size = m - 1;
-		else
-			return (m);
+		while (first <= last)
+		{
+			print_array(array, first, last);
+			i = (first + last) / 2;
+
+			if (value > array[i])
+				first = i + 1;
+			else if (value < array[i])
+				last = i - 1;
+			else
+				return (i);
+		}
 	}
+
 	return (-1);
 }
